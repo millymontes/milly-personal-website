731139ace6020430be67ef53802f9b3024d71ca9
millymontes <millybriannaa@gmail.com>
2026-02-10 10:34:43 -0700

    installed correct vite folders

---
+++ personal-website/App.jsx
@@ -0,0 +1,10 @@
+//important imports
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import AboutMe from "../src/pages/AboutMe";
+import Projects from "../src/pages/Projects";
+import Contact from "../src/pages/Contact";
+import Error404 from "./Error404";
+import "./App.css";
+
+// come back for export default app
--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-//important imports
-import { Routes, Route } from "react-router";
-import AboutMe from "./pages/AboutMe";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Error404 from "./Error404";
