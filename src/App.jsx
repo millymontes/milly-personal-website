9f70e5740d0212565b724b66dca98eba5ac92a26
millymontes <millybriannaa@gmail.com>
2026-02-09 22:21:15 -0700

    doing important imports in App folder

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+//important imports
+import { Routes, Route } from "react-router";
+import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Error404 from "./Error404";
