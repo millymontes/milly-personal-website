448cc4242f1343d8105a756d3158293a9926c554
millymontes <millybriannaa@gmail.com>
2026-05-20 10:22:28 -0600

    adding projects info

--- personal-website/pages/Home.jsx
+++ personal-website/pages/Home.jsx
@@ -3,6 +3,8 @@ import "../src/App.css";
 function Home() {
   return (
     <section id="home" style={{ height: "100vh" }}>
+      <p className="home-section">Aspiring Front-End Developer</p>
+      <p className="home-section">Certified Software Engineer</p>
       <h1 className="namelogo">Milly</h1>
     </section>
   );
--- personal-website/pages/Projects.jsx
+++ personal-website/pages/Projects.jsx
@@ -5,6 +5,12 @@ function Projects() {
     <section id="projects" style={{ height: "100vh" }}>
       <NavBar />
       <h1>Projects</h1>
+      {/* // add project descriptions and links to github repos -- add css */}
+      <section className="project-list">
+        <h3>Blueberry Game</h3>
+        <h3>Project 2</h3>
+        <h3>Project 3</h3>
+      </section>
     </section>
   );
 }
