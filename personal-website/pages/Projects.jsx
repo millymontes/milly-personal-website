ed6e457f8feb4c64b80554ec0cc7f12501cd0121
millymontes <millybriannaa@gmail.com>
2026-06-03 15:47:54 -0600

    vague codes to progress with the website -- will be doing more complex coding to fix everything -- this is like a rough draft of the rough draft

--- personal-website/pages/Projects.jsx
+++ personal-website/pages/Projects.jsx
@@ -6,10 +6,10 @@ function Projects() {
       <NavBar />
       <h1>Projects</h1>
       {/* // add project descriptions and links to github repos -- add css */}
-      <section className="project-list">
-        <h3>Blueberry Game</h3>
-        <h3>Project 2</h3>
-        <h3>Project 3</h3>
+      <section id="project-list">
+        <h3 className="project-list1">Blueberry Game</h3>
+        <h3 className="project-list2">Project 2</h3>
+        <h3 className="project-list3">Project 3</h3>
       </section>
     </section>
   );
